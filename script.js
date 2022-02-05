e0eb8ac57e4daf12d589fb5535cfcc930c802b69
hansen-ri <gridironhero@gmail.com>
2022-02-05 12:58:29 -0700

    20 Points

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ require([
     type: "simple-marker",
     color: [226, 119, 40], // Orange
     outline: {
-      color: [0, 0, 0], // White
+      color: [255, 255, 255], // Black
       width: 1,
     },
   };
@@ -42,6 +42,406 @@ require([
     symbol: simpleMarkerSymbol,
   });
   graphicsLayer.add(pointGraphic);
+
+  const point2 = {
+    //Create a 2nd point
+    type: "point",
+    longitude: -111.789560,
+    latitude: 43.824918,
+  };
+  const simpleMarkerSymbol2 = {
+    type: "simple-marker",
+    color: [226, 119, 40], // Orange
+    outline: {
+      color: [255, 255, 255], // Black
+      width: 1,
+    },
+  };
+
+  const pointGraphic2 = new Graphic({
+    geometry: point2,
+    symbol: simpleMarkerSymbol2,
+  });
+  graphicsLayer.add(pointGraphic2);
+
+  const point3 = {
+    //Create a 3rd point
+    type: "point",
+    longitude: -111.774267,
+    latitude: 43.855087,
+  };
+  const simpleMarkerSymbol3 = {
+    type: "simple-marker",
+    color: [226, 119, 40], // Orange
+    outline: {
+      color: [255, 255, 255], // black
+      width: 1,
+    },
+  };
+
+  const pointGraphic3 = new Graphic({
+    geometry: point3,
+    symbol: simpleMarkerSymbol3,
+  });
+  graphicsLayer.add(pointGraphic3);
+
+  const point4 = {
+    //Create a 4th point
+    type: "point",
+    longitude: -111.771536,
+    latitude: 43.848675,
+  };
+  const simpleMarkerSymbol4 = {
+    type: "simple-marker",
+    color: [226, 119, 40], // Orange
+    outline: {
+      color: [255, 255, 255], // black
+      width: 1,
+    },
+  };
+
+  const pointGraphic4 = new Graphic({
+    geometry: point4,
+    symbol: simpleMarkerSymbol4,
+  });
+  graphicsLayer.add(pointGraphic4);
+
+  const point5 = {
+    //Create a 5th point
+    type: "point",
+    longitude: -111.777979,
+    latitude: 43.842871,
+  };
+  const simpleMarkerSymbol5 = {
+    type: "simple-marker",
+    color: [226, 119, 40], // Orange
+    outline: {
+      color: [255, 255, 255], // black
+      width: 1,
+    },
+  };
+
+  const pointGraphic5 = new Graphic({
+    geometry: point5,
+    symbol: simpleMarkerSymbol5,
+  });
+  graphicsLayer.add(pointGraphic5);
+
+  const point6 = {
+    //Create a 6th point
+    type: "point",
+    longitude: -111.779498,
+    latitude: 43.837371,
+  };
+  const simpleMarkerSymbol6 = {
+    type: "simple-marker",
+    color: [226, 119, 40], // Orange
+    outline: {
+      color: [255, 255, 255], // black
+      width: 1,
+    },
+  };
+
+  const pointGraphic6 = new Graphic({
+    geometry: point6,
+    symbol: simpleMarkerSymbol6,
+  });
+  graphicsLayer.add(pointGraphic6);
+
+  const point7 = {
+    //Create a 7th point
+    type: "point",
+    longitude: -111.780363,
+    latitude: 43.836130,
+  };
+  const simpleMarkerSymbol7 = {
+    type: "simple-marker",
+    color: [226, 119, 40], // Orange
+    outline: {
+      color: [255, 255, 255], // black
+      width: 1,
+    },
+  };
+
+  const pointGraphic7 = new Graphic({
+    geometry: point7,
+    symbol: simpleMarkerSymbol7,
+  });
+  graphicsLayer.add(pointGraphic7);
+
+  const point8 = {
+    //Create a 8th point
+    type: "point",
+    longitude: -111.778033,
+    latitude: 43.833523,
+  };
+  const simpleMarkerSymbol8 = {
+    type: "simple-marker",
+    color: [226, 119, 40], // Orange
+    outline: {
+      color: [255, 255, 255], // black
+      width: 1,
+    },
+  };
+
+  const pointGraphic8 = new Graphic({
+    geometry: point8,
+    symbol: simpleMarkerSymbol8,
+  });
+  graphicsLayer.add(pointGraphic8);
+
+  const point9 = {
+    //Create a 9th point
+    type: "point",
+    longitude: -111.778887,
+    latitude: 43.832891,
+  };
+  const simpleMarkerSymbol9 = {
+    type: "simple-marker",
+    color: [226, 119, 40], // Orange
+    outline: {
+      color: [255, 255, 255], // black
+      width: 1,
+    },
+  };
+
+  const pointGraphic9 = new Graphic({
+    geometry: point9,
+    symbol: simpleMarkerSymbol9,
+  });
+  graphicsLayer.add(pointGraphic9);
+
+
+  const point10 = {
+    //Create a 10th point
+    type: "point",
+    longitude: -111.777481,
+    latitude: 43.830357,
+  };
+  const simpleMarkerSymbol10= {
+    type: "simple-marker",
+    color: [226, 119, 40], // Orange
+    outline: {
+      color: [255, 255, 255], // white
+      width: 1,
+    },
+  };
+
+  const pointGraphic10 = new Graphic({
+    geometry: point10,
+    symbol: simpleMarkerSymbol10,
+  });
+  graphicsLayer.add(pointGraphic10);
+
+  const point11 = {
+    //Create a 11th point
+    type: "point",
+    longitude: -111.777481,
+    latitude: 43.830357,
+  };
+  const simpleMarkerSymbol11= {
+    type: "simple-marker",
+    color: [226, 119, 40], // Orange
+    outline: {
+      color: [255, 255, 255], // white
+      width: 1,
+    },
+  };
+
+  const pointGraphic11 = new Graphic({
+    geometry: point11,
+    symbol: simpleMarkerSymbol11,
+  });
+  graphicsLayer.add(pointGraphic11);
+
+  const point12 = {
+    //Create a 12th point
+    type: "point",
+    longitude: -111.783497,
+    latitude: 43.824918,
+  };
+  const simpleMarkerSymbol12= {
+    type: "simple-marker",
+    color: [226, 119, 40], // Orange
+    outline: {
+      color: [255, 255, 255], // white
+      width: 1,
+    },
+  };
+
+  const pointGraphic12 = new Graphic({
+    geometry: point12,
+    symbol: simpleMarkerSymbol12,
+  });
+  graphicsLayer.add(pointGraphic12);
+
+  const point13 = {
+    //Create a 13th point
+    type: "point",
+    longitude: -111.784977,
+    latitude: 43.824115,
+  };
+  const simpleMarkerSymbol13= {
+    type: "simple-marker",
+    color: [226, 119, 40], // Orange
+    outline: {
+      color: [255, 255, 255], // white
+      width: 1,
+    },
+  };
+
+  const pointGraphic13 = new Graphic({
+    geometry: point13,
+    symbol: simpleMarkerSymbol13,
+  });
+  graphicsLayer.add(pointGraphic13);
+
+  const point14 = {
+    //Create a 14th point
+    type: "point",
+    longitude: -111.789316,
+    latitude: 43.821829,
+  };
+  const simpleMarkerSymbol14= {
+    type: "simple-marker",
+    color: [226, 119, 40], // Orange
+    outline: {
+      color: [255, 255, 255], // white
+      width: 1,
+    },
+  };
+
+  const pointGraphic14 = new Graphic({
+    geometry: point14,
+    symbol: simpleMarkerSymbol14,
+  });
+  graphicsLayer.add(pointGraphic14);
+
+  const point15 = {
+    //Create a 15th point
+    type: "point",
+    longitude: -111.785934,
+    latitude: 43.821949,
+  };
+  const simpleMarkerSymbol15= {
+    type: "simple-marker",
+    color: [226, 119, 40], // Orange
+    outline: {
+      color: [255, 255, 255], // white
+      width: 1,
+    },
+  };
+
+  const pointGraphic15 = new Graphic({
+    geometry: point15,
+    symbol: simpleMarkerSymbol15,
+  });
+  graphicsLayer.add(pointGraphic15);
+
+  const point16 = {
+    //Create a 16th point
+    type: "point",
+    longitude: -111.789705,
+    latitude: 43.820129,
+  };
+  const simpleMarkerSymbol16= {
+    type: "simple-marker",
+    color: [226, 119, 40], // Orange
+    outline: {
+      color: [255, 255, 255], // white
+      width: 1,
+    },
+  };
+
+  const pointGraphic16 = new Graphic({
+    geometry: point16,
+    symbol: simpleMarkerSymbol16,
+  });
+  graphicsLayer.add(pointGraphic16);
+
+  const point17 = {
+    //Create a 17th point
+    type: "point",
+    longitude: -111.789176,
+    latitude: 43.817901,
+  };
+  const simpleMarkerSymbol17= {
+    type: "simple-marker",
+    color: [226, 119, 40], // Orange
+    outline: {
+      color: [255, 255, 255], // white
+      width: 1,
+    },
+  };
+
+  const pointGraphic17 = new Graphic({
+    geometry: point17,
+    symbol: simpleMarkerSymbol17,
+  });
+  graphicsLayer.add(pointGraphic17);
+
+  const point18 = {
+    //Create a 18th point
+    type: "point",
+    longitude: -111.804143,
+    latitude: 43.812842,
+  };
+  const simpleMarkerSymbol18= {
+    type: "simple-marker",
+    color: [226, 119, 40], // Orange
+    outline: {
+      color: [255, 255, 255], // white
+      width: 1,
+    },
+  };
+
+  const pointGraphic18 = new Graphic({
+    geometry: point18,
+    symbol: simpleMarkerSymbol18,
+  });
+  graphicsLayer.add(pointGraphic18);
+
+  const point19 = {
+    //Create a 19th point
+    type: "point",
+    longitude: -111.777557,
+    latitude: 43.810984,
+  };
+  const simpleMarkerSymbol19= {
+    type: "simple-marker",
+    color: [226, 119, 40], // Orange
+    outline: {
+      color: [255, 255, 255], // white
+      width: 1,
+    },
+  };
+
+  const pointGraphic19 = new Graphic({
+    geometry: point19,
+    symbol: simpleMarkerSymbol19,
+  });
+  graphicsLayer.add(pointGraphic19);
+
+  const point20 = {
+    //Create a 20th point
+    type: "point",
+    longitude: -111.810928,
+    latitude: 43.804823,
+  };
+  const simpleMarkerSymbol20= {
+    type: "simple-marker",
+    color: [226, 119, 40], // Orange
+    outline: {
+      color: [255, 255, 255], // white
+      width: 1,
+    },
+  };
+
+  const pointGraphic20 = new Graphic({
+    geometry: point20,
+    symbol: simpleMarkerSymbol20,
+  });
+  graphicsLayer.add(pointGraphic20);
 
   const polyline = {
     type: "polyline",
@@ -160,7 +560,6 @@ require([
       [-111.8279515, 43.818843], //Longitude, latitude 73
       [-111.8180205, 43.818843], //Longitude, latitude 74
       [-111.817961, 43.826055], //Longitude, latitude 75
-
       [-111.810216, 43.826100], //Longitude, latitude 76
       [-111.810648, 43.836929], //Longitude, latitude 77
       [-111.818048, 43.836929], //Longitude, latitude 78
@@ -169,7 +568,7 @@ require([
 
   const simpleFillSymbol = {
     type: "simple-fill",
-    color: [227, 139, 79, 0.15], // Orange, opacity 30%
+    color: [227, 139, 79, 0.15], // Orange, opacity 15%
     outline: {
       color: [255, 153, 55],
       width: 1,
